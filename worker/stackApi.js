233798adfe6654437b464d8c80d024a95d4cff07
John Rosewicz <john.rosewicz@gmail.com>
2017-02-04 23:07:24 +0000

    Getting posts recently edited by someone other than their author

--- worker/stackApi.js
+++ worker/stackApi.js
@@ -1,14 +1,31 @@
 var https = require("https");
 var zlib = require("zlib");
+var sleep = require('sleep');
 
 var baseUrl = "https://api.stackexchange.com/2.2/";
 
-function getAllRecentlyEditedPosts(postIds, pageNumber) {
+function getAllRecentlyEditedPosts() {
+    getAllRecentlyEditedPostEvents()
+        .then(function (postIds) {
+            var postsPath = `posts/${postIds}?pagesize=100&order=desc&sort=activity&site=stackoverflow&filter=!0S2DU84KBii0TP2pzjFB-BAaU`;
+            var url = baseUrl + postsPath;
+            
+            queryStackExchangeApi(url).then(function (results) {
+               results.items = results.items.filter(function (item) {
+                  return item.owner.user_id !== item.last_editor.user_id; 
+               });
+               
+               console.log(results);
+            });
+        });
+}
+
+function getAllRecentlyEditedPostEvents(postIds, pageNumber) {
     
     pageNumber = pageNumber || 1;
     postIds = postIds || [];
     
-    getRecentlyEditedPosts(pageNumber)
+    return getRecentlyEditedPostEvents(pageNumber)
         .then(function (results) {
             postIds = postIds.concat(results.items
                 .filter(function (item) {
@@ -21,31 +38,24 @@ function getAllRecentlyEditedPosts(postIds, pageNumber) {
                 // We set a timeout here to make sure we don't violate the backoff value if it is returned in the results.
                 // Having backoff set means the application must not make the same request for that number of seconds.
                 // https://api.stackexchange.com/docs/throttle
-                setTimeout(function () {
+                //setTimeout(function () {
                     // recursively call to get around annoying promises in loops
-                    getAllRecentlyEditedPosts(postIds, pageNumber + 1)
-                }, results.backoff * 1000);
+                    return getAllRecentlyEditedPostEvents(postIds, pageNumber + 1);
+                //}, results.backoff * 1000);
             } else {
-                console.log(pageNumber);
-                console.log(postIds.join(";"));
+                return postIds.join(";");
             }
         });
 }
 
-function getRecentlyEditedPosts(pageNumber) {
+function getRecentlyEditedPostEvents(pageNumber) {
     var pageSize = 100;
-    
-    // Store our access token in an environment variable.
-    var accessToken = process.env.STACK_ACCESS_TOKEN;
-    
-    // This is not a secret value.
-    var key = "cqqgtR6JWKsaLYQN2N6))Q((";
     
     // This value represents the fields we want to get back from this query.
     // These are immutable and safe to hard code: https://api.stackexchange.com/docs/filters
     var filter = "!9YdnSOd9y";
     
-    var eventsPath = `events?page=${pageNumber}&pagesize=${pageSize}&site=stackoverflow&filter=${filter}&access_token=${accessToken}&key=${key}`;
+    var eventsPath = `events?page=${pageNumber}&pagesize=${pageSize}&site=stackoverflow&filter=${filter}`;
     var url = baseUrl + eventsPath;
     
     return new Promise(function (resolve, reject) {
@@ -60,9 +70,18 @@ function getRecentlyEditedPosts(pageNumber) {
 }
 
 function queryStackExchangeApi(url) {
+    
+    // This is not a secret value.
+    var key = "cqqgtR6JWKsaLYQN2N6))Q((";
+    
+    // Store our access token in an environment variable.
+    var accessToken = process.env.STACK_ACCESS_TOKEN;
+    
+    url += `&key=${key}&access_token=${accessToken}`;
+    
     return new Promise(function (resolve, reject) {
         // https://gist.github.com/ORESoftware/9d27e3ed392e2eefb123
-        console.log(`Querying ${url}`);
+        //console.log(`Querying ${url}`);
         return getGzipped(url, function (err, data) {
             if (err) {
                 reject(Error(err && err.error_message));
