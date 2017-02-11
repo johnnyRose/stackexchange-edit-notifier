35183c6d74086ef3373b64d1c9c13cb8b8750631
John Rosewicz <john.rosewicz@gmail.com>
2017-02-11 06:08:14 +0000

    Simple web interface to subscribe users

--- web/app.js
+++ web/app.js
@@ -4,12 +4,16 @@ var express = require('express');
 var app = express();
 var pug = require('pug');
 var path = require('path');
+var bodyParser = require('body-parser');
 
 var homeRoute = require('./routes/home.js');
 var worker = require('../worker/worker');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // start background worker
-worker.start();
+//worker.start();
 
 // set the view engine to pug
 app.set('view engine', 'pug');
@@ -20,22 +24,10 @@ app.set('views', path.join(__dirname, '/views'));
 // configure routes
 app.use('/', homeRoute);
 
-
 var knex = require('knex');
 var knexFile = require('../knexfile');
 
-var user = {
-    email: 'jmr1792@yahoo.com',
-    userId: 2840103,
-    site: 'stackoverflow',
-    insertDate: new Date()
-};
-
 var db = knex(knexFile.development);
-
-//db('users').insert(user).then(function () {
-
-//});
 
 db('users').select().then(function (results) {
     console.log(results);
--- web/routes/home.js
+++ web/routes/home.js
@@ -1,8 +1,26 @@
-
+var knex = require("knex");
+var knexFile = require("../../knexfile");
+var db = knex(knexFile.development);
 var router = require("express-promise-router")();
 
 router.get('/', function (request, response) {
-   response.render('home');
+    response.render('home');
+});
+
+router.post('/', function (request, response) {
+    console.log(request.body);
+    var user = {
+        email: request.body.email,
+        userId: request.body.userId,
+        site: request.body.site,
+        insertDate: new Date(),
+    };
+
+    db('users').insert(user).then(function () {
+        db('users').select().then(function (r) {
+            console.log(r);
+        })
+    });
 });
 
 module.exports = router;
